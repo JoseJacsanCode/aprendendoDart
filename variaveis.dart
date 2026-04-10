17be1e37fe5b4f34043dcef16aa10e23cd216ce6
Jose Jacsan <jjacsan@gmail.com>
2026-04-10 15:30:39 -0300

    Começando com Dart

---
+++ variaveis.dart
@@ -0,0 +1,12 @@
+void main() {
+  String olaMundo = "Olá Mundo!";
+  int valor = 15;
+  bool ehVerdadeiro = true;
+  List<String> listaDePalavras = ["Jose", "Jacsan", "Sousa", "Pereira"];
+
+  print(olaMundo);
+  print(valor);
+  print(ehVerdadeiro);
+  print(listaDePalavras);
+  print('${listaDePalavras[0]} - ${listaDePalavras[1]}');
+}
