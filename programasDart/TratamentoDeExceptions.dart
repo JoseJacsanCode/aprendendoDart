1c09bc9d26e04dd67124ff4b035c61e331fcd785
Jose Jacsan <jjacsan@gmail.com>
2026-04-15 17:21:17 -0300

    Estudando exceções

---
+++ programasDart/TratamentoDeExceptions.dart
@@ -0,0 +1,43 @@
+void main() {
+  // try {
+  //   print(2 ~/ 0);
+  // } catch (e) {
+  //   print('Printando o erro $e');
+  //   // rethrow; //propagando o erro
+  //   // throw Exception('Ocorreu um erro xpto'); //retornar um novo erro
+  //   throw CustomError('Erro customizado');
+  // }
+
+  // String user = 'admin';
+  // String pass = '123';
+
+  // if(pass.length <= 6) throw PasswordLenghtError();
+
+  Login login = Login();
+  try {
+    login.logar();
+
+  } on PasswordLenghtError catch (e) {
+    print('Falhou ao logar');
+  }
+  catch(e) {
+    print('Outro erro!');
+  }
+  finally{
+    print('Finalizou');
+  }
+
+}
+
+class Login() {
+  void logar() {
+    //String user = 'admin';
+    String pass = '123';
+
+    if(pass.length <= 6) throw Exception();
+  }
+}
+
+class PasswordLenghtError implements Exception {
+  PasswordLenghtError();
+}
